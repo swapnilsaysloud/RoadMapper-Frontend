26c0d20927419b6120979f2eba8176ce24500e26
Swapnil <swapnilsaraswat101@gmail.com>
2024-12-27 14:25:48 +0530

    added proxy

--- vite.config.js
+++ vite.config.js
@@ -2,5 +2,14 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  server: {
+    proxy: {
+      '/api': {
+        target: 'https://roadmapper-backend.onrender.com',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, '')
+      }
+    }
+  },
   plugins: [react()],
 })
