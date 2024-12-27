632b76d647e619251282ca2c7013336dbe890167
Swapnil Saraswat <110056216+swapnilsaysloud@users.noreply.github.com>
2024-12-27 20:38:02 +0530

    Rollback last commit

--- src/components/Runner.jsx
+++ src/components/Runner.jsx
@@ -9,21 +9,21 @@ const Runner = ({ gifUrl }) => {
 
       // So the free tier shuts down the server after 20 minutes of inactivity, so adding this to keep the server alive
   
-      const url = "https://roadmapper-backend-1.onrender.com"; // Replace with your Render URL
-      const interval = 60000; // Interval in milliseconds (30 seconds)
+      // const url = "https://roadmapper-backend-1.onrender.com"; // Replace with your Render URL
+      // const interval = 60000; // Interval in milliseconds (30 seconds)
   
-      function reloadWebsite() {
-        axios.get(url)
-          .then(response => {
-            console.log(`Reloaded at ${new Date().toISOString()}: Status Code ${response.status}`);
-          })
-          .catch(error => {
-            console.error(`Error reloading at ${new Date().toISOString()}:`, error.message);
-          });
-      }
+      // function reloadWebsite() {
+      //   axios.get(url)
+      //     .then(response => {
+      //       console.log(`Reloaded at ${new Date().toISOString()}: Status Code ${response.status}`);
+      //     })
+      //     .catch(error => {
+      //       console.error(`Error reloading at ${new Date().toISOString()}:`, error.message);
+      //     });
+      // }
   
   
-      setInterval(reloadWebsite, interval);
+      // setInterval(reloadWebsite, interval);
 
     
   useEffect(() => {
